df9a4da09068c35bff59e6ca47d14631acc31c40
Sachin Karia <sachin.karia01@gmail.com>
2016-12-11 22:01:29 +0000

    signup page

---
+++ www/js/CardsService.js
@@ -0,0 +1,18 @@
+starter.service('CardsService', ['$http', function($http){
+  var self = this;
+
+  self.getCards = function(){
+    return $http.get("http://star-notes.herokuapp.com/")
+  };
+
+  self.createCard = function(card){
+    return $http.post("http://star-notes.herokuapp.com/cards.json", card)
+  };
+
+  self.destroyCard = function(id){
+    $http.delete("http://star-notes.herokuapp.com/" + id).success(function(data){
+      console.log(data)
+    });
+  };
+
+}]);
---
+++ www/js/MainCtrl.js
@@ -0,0 +1,55 @@
+starter.controller('MainCtrl', ['$scope','$auth','CardsService',function($scope, $auth, CardsService){
+  this.signedUp = false;
+  $scope.cards = [];
+  $scope.current_user = $auth.user;
+  $scope.registrationForm = {};
+
+  $scope.handleRegBtnClick = function() {
+    $auth.submitRegistration($scope.registrationForm)
+    .then(function() {
+      console.log('SUCCESS')
+    })
+    .catch(function(){
+      console.log('FAILED')
+    })
+  };
+
+  $scope.handleLoginBtnClick = function() {
+    $auth.submitLogin($scope.registrationForm)
+    .then(function(resp) {
+      console.log('SUCCESS');
+    })
+    .catch(function(resp) {
+      console.log('FAILED.');
+    });
+  };
+
+  $scope.handleSignOutBtnClick = function() {
+  $auth.signOut()
+    .then(function(resp) {
+      console.log('SUCCESS');
+    })
+    .catch(function(resp) {
+      console.log('FAILED.');
+    });
+};
+
+  $scope.signUp = function(){
+    $scope.signedUp = true;
+  };
+
+  $scope.createCard = function(card){
+    CardsService.createCard(card).then(function(){
+      $scope.getCards();
+    });
+  }
+
+  $scope.getCards = function(){
+  return CardsService.getCards().then(function(response){
+    $scope.cards = response.data;
+    console.log($scope.cards);
+  });
+};
+
+  $scope.getCards();
+}]);
